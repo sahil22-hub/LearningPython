bf1592d6e66e12a98820d26e2a34e39b241f287b
sahil <sahilbhetwal22@gmail.com>
2023-07-06 16:53:19 +0545

    first

---
+++ One/mario_less/mario.py
@@ -0,0 +1,13 @@
+
+while True:
+    height = int(input("Height: "))
+    width = height + 1
+    if height >= 0 and height <= 23:
+        break
+
+for i in range(1, height + 1):
+    num_hashes = i + 1
+    num_spaces = width - num_hashes
+
+    print(" " * num_spaces, end="")
+    print("#" * num_hashes)
