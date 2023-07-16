16c03d539424fbffa538f6187ff796a2e8eef419
sahil <sahilbhetwal22@gmail.com>
2023-07-17 00:01:16 +0545

    h

--- test/test.py
+++ test/test.py
@@ -24,16 +24,8 @@ user_data = [('John Doe', 25), ('Jane Smith', 30)]
 cursor.executemany(insert_data_query, user_data)
 
 # Retrieve data from the table
-select_data_query = '''
-    SELECT * FROM users;
-'''
-cursor.execute(select_data_query)
-result = cursor.fetchall()
+sele
 
-# Print the retrieved data
-for row in result:
-    print(f'ID: {row[0]}, Name: {row[1]}, Age: {row[2]}')
-
-# Commit the changes and close the connection
-conn.commit()
-conn.close()
+# # Commit the changes and close the connection
+# conn.commit()
+# conn.close()
